a01739ba360fa936e4882c3e365e3b00f6069f12
Prosperedafe <mamusedafe19@gmail.com>
2022-11-21 11:08:51 +0100

    navbar responsive

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const toggleButton = document.getElementsByClassName('open-nav')
-const navbarLinks = document.getElementsByClassName('header__nav__ul')
+const toggleButton = document.querySelector('.open-nav')
+const navbarLinks = document.querySelector('.header__nav__ul')
+const closeBtn = document.querySelector(".close-nav")
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.style.display = "block"
+})
+
+closeBtn.addEventListener("click", () => {
+  navbarLinks.style.display = "none"
+})
 
 let slideIndex = 0;
 showSlides();
